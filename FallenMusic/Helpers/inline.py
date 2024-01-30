269c76d0b002d6fcff6b3401aed0e2938728a9ed
Aliiyp <157922994+Aliiyp@users.noreply.github.com>
2024-01-30 19:06:32 +0300

    inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -59,7 +59,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="• مطوࢪ السورس •", url="https://t.me/G_7_Rr"
+            text="• مطوࢪ السورس •", url="https://t.me/iTsFlNe"
         ),
         InlineKeyboardButton(text="• مالك البوت •", user_id=config.OWNER_ID),
     ],
@@ -79,7 +79,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="• مطوࢪ السورس •", url="https://t.me/G_7_Rr"
+            text="• مطوࢪ السورس •", url="https://t.me/iTsFlNe"
         ),
         InlineKeyboardButton(text="• مالك البوت •", user_id=config.OWNER_ID),
     ],
